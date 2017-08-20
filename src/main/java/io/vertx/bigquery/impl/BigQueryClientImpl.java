cc7d37f27b70d7c925c1cf14302171ddd2195317
Thibault Le Ouay <thibaultleouay@gmail.com>
2017-08-20 19:30:42 +0200

    remove deadcode

--- src/main/java/io/vertx/bigquery/impl/BigQueryClientImpl.java
+++ src/main/java/io/vertx/bigquery/impl/BigQueryClientImpl.java
@@ -46,14 +46,14 @@ public class BigQueryClientImpl implements BigQueryClient {
 	@Override
 	public BigQueryClient deleteDataset(String projectId, String datasetId, Handler<AsyncResult<Void>> succesHandler) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/" + datasetId;
-		createDeleteRequest(path, succesHandler, Void.class);
+		createDeleteRequest(path, succesHandler);
 		return this;
 	}
 
 	@Override
 	public BigQueryClient getDataset(String projectId, String datasetId, Handler<AsyncResult<Dataset>> succesHandler) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/" + datasetId;
-		createGetRequestWithOptions(path, succesHandler, Dataset.class, null);
+		createGetRequestWithOptions(path, succesHandler, null);
 		return this;
 
 	}
@@ -62,7 +62,7 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient insertDataset(String projectId, Handler<AsyncResult<Dataset>> succesHandler,
 			Dataset payload) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/";
-		createPostRequest(path, succesHandler, Dataset.class, payload);
+		createPostRequest(path, succesHandler,  payload);
 		return this;
 	}
 
@@ -76,7 +76,7 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient listDatasetsWithOptions(String projectId, Handler<AsyncResult<DatasetList>> succesHandler,
 			JsonObject options) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/";
-		createGetRequestWithOptions(path, succesHandler, DatasetList.class, options);
+		createGetRequestWithOptions(path, succesHandler, options);
 		return this;
 	}
 
@@ -84,7 +84,7 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient patchDataset(String projectId, String datasetId, Handler<AsyncResult<Dataset>> succesHandler,
 			Dataset payload) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/" + datasetId;
-		createPatchRequest(path, succesHandler, Dataset.class, payload);
+		createPatchRequest(path, succesHandler, payload);
 		return this;
 	}
 
@@ -92,7 +92,7 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient updateDataSet(String projectId, String datasetId, Handler<AsyncResult<Dataset>> succesHandler,
 			Dataset payload) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/" + datasetId;
-		createPutRequest(path, succesHandler, Dataset.class, payload);
+		createPutRequest(path, succesHandler,  payload);
 		return this;
 	}
 
@@ -100,14 +100,14 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient cancelJobs(String projectId, String jobId,
 			Handler<AsyncResult<JobCancelResponse>> successHandler) {
 		String path = DEFAULT_ENDPOINT + projectId + "/jobs/" + jobId + "/cancel";
-		createGetRequest(path, successHandler, JobCancelResponse.class);
+		createGetRequest(path, successHandler);
 		return this;
 	}
 
 	@Override
 	public BigQueryClient getJob(String projectId, String jobId, Handler<AsyncResult<Job>> succesHandler) {
 		String path = DEFAULT_ENDPOINT + projectId + "/jobs/" + jobId ;
-		createGetRequest(path, succesHandler, Job.class);
+		createGetRequest(path, succesHandler);
 		return this;
 	}
 	
@@ -123,7 +123,7 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient getQueryResultWithOptions(String projectId, String jobId,
 			Handler<AsyncResult<GetQueryResultsResponse>> succesHandler, JsonObject options) {
 		String path = DEFAULT_ENDPOINT + projectId + "/queries/" + jobId ;
-		createGetRequestWithOptions(path, succesHandler, GetQueryResultsResponse.class, options);
+		createGetRequestWithOptions(path, succesHandler, options);
 		return this;
 	}
 	
@@ -136,7 +136,7 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient listJobsWithOptions(String projectId, Handler<AsyncResult<JobList>> succeshandler,
 			JsonObject options) {
 		String path  = DEFAULT_ENDPOINT + projectId + "/jobs";
-		createGetRequestWithOptions(path, succeshandler, JobList.class, options);
+		createGetRequestWithOptions(path, succeshandler, options);
 		return this;
 	}
 	
@@ -144,13 +144,13 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient queryJob(String projectId, Handler<AsyncResult<QueryResponse>> succeshandler,
 			QueryRequest payload) {
 		String path  = DEFAULT_ENDPOINT + projectId + "/queries";
-		createPostRequest(path, succeshandler, QueryResponse.class, payload);
+		createPostRequest(path, succeshandler, payload);
 		return this;
 	}
 	
 	@Override
 	public BigQueryClient listProject(Handler<AsyncResult<ProjectList>> succeshandler) {
-		createGetRequest(DEFAULT_ENDPOINT, succeshandler, ProjectList.class);
+		createGetRequest(DEFAULT_ENDPOINT, succeshandler);
 		return this;
 	}
 	
@@ -158,7 +158,7 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient insertAllTableData(String projectId, String datasetId, String tableId,
 			Handler<AsyncResult<TableDataInsertAllResponse>> succeshandler, TableDataInsertAllRequest payload) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/" + datasetId + "/tables/" + tableId+ "/insertAll";
-		createPostRequest(path, succeshandler, TableDataInsertAllResponse.class, payload);
+		createPostRequest(path, succeshandler,  payload);
 		return this;
 	}
 	
@@ -166,7 +166,7 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient deleteTable(String projectId, String datasetId, String tableId,
 			Handler<AsyncResult<Void>> succeHandler) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/" + datasetId + "/tables/" + tableId;
-		createDeleteRequest(path, succeHandler, Void.class);
+		createDeleteRequest(path, succeHandler);
 		return this;
 	}
 
@@ -181,7 +181,7 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient getTableWithOptions(String projectId, String datasetId, String tableId,
 			Handler<AsyncResult<Table>> succeshandler, JsonObject options) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/" + datasetId + "/tables/" + tableId;
-		createGetRequestWithOptions(path, succeshandler, Table.class, options);
+		createGetRequestWithOptions(path, succeshandler,  options);
 		return this;
 	}
 
@@ -189,7 +189,7 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient insertTable(String projectId, String datasetId, Handler<AsyncResult<Table>> succeshandler,
 			Table payload) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/" + datasetId + "/tables/";
-		createPostRequest(path, succeshandler, Table.class, payload);
+		createPostRequest(path, succeshandler, payload);
 		return this;
 	}
 
@@ -204,7 +204,7 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient listTablesWithOptions(String projectId, String datasetId,
 			Handler<AsyncResult<TableList>> succeshandler, JsonObject options) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/" + datasetId + "/tables/";
-		createGetRequestWithOptions(path, succeshandler, TableList.class, options);
+		createGetRequestWithOptions(path, succeshandler, options);
 		return this;
 	}
 
@@ -212,7 +212,7 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient patchTable(String projectId, String datasetId, String tableId,
 			Handler<AsyncResult<Table>> succeshandler, Table payload) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/" + datasetId + "/tables/" + tableId;
-		createPatchRequest(path, succeshandler, Table.class, payload);
+		createPatchRequest(path, succeshandler,  payload);
 		return this;
 	}
 
@@ -220,54 +220,54 @@ public class BigQueryClientImpl implements BigQueryClient {
 	public BigQueryClient updateTable(String projectId, String datasetId, String tableId,
 			Handler<AsyncResult<Table>> succeshandler, Table payload) {
 		String path = DEFAULT_ENDPOINT + projectId + "/datasets/" + datasetId + "/tables/" + tableId;
-		createPutRequest(path, succeshandler, Table.class, payload);
+		createPutRequest(path, succeshandler, payload);
 		return this;
 	}
 
-	private <T> void createDeleteRequest(String path, Handler<AsyncResult<T>> handler, Class<T> expectedClass) {
-		request(HttpMethod.DELETE, path, handler, expectedClass, null, null);
+	private <T> void createDeleteRequest(String path, Handler<AsyncResult<T>> handler) {
+		request(HttpMethod.DELETE, path, handler,  null, null);
 	}
 
-	private <T> void createGetRequest(String path, Handler<AsyncResult<T>> handler, Class<T> expectedClass) {
-		request(HttpMethod.GET, path, handler, expectedClass, null, null);
+	private <T> void createGetRequest(String path, Handler<AsyncResult<T>> handler) {
+		request(HttpMethod.GET, path, handler,  null, null);
 	}
 
-	private <T> void createGetRequestWithOptions(String path, Handler<AsyncResult<T>> handler, Class<T> expectedClass,
+	private <T> void createGetRequestWithOptions(String path, Handler<AsyncResult<T>> handler,
 			JsonObject queryParam) {
-		request(HttpMethod.GET, path, handler, expectedClass, null, queryParam);
+		request(HttpMethod.GET, path, handler,  null, queryParam);
 	}
 
-	private <T> void createPatchRequest(String path, Handler<AsyncResult<T>> handler, Class<T> expectedClass,
+	private <T> void createPatchRequest(String path, Handler<AsyncResult<T>> handler,
 			Object payload) {
-		request(HttpMethod.PATCH, path, handler, expectedClass, payload, null);
-
-	}
-
-	private <T> void createPostRequest(String path, Handler<AsyncResult<T>> handler, Class<T> expectedClass,
-			Object payload) {
-		request(HttpMethod.POST, path, handler, expectedClass, payload, null);
+		request(HttpMethod.PATCH, path, handler,  payload, null);
 
 	}
 
-	private <T> void createPutRequest(String path, Handler<AsyncResult<T>> handler, Class<T> expectedClass,
+	private <T> void createPostRequest(String path, Handler<AsyncResult<T>> handler,
 			Object payload) {
-		request(HttpMethod.PUT, path, handler, expectedClass, payload, null);
+		request(HttpMethod.POST, path, handler,  payload, null);
+
+	}
+
+	private <T> void createPutRequest(String path, Handler<AsyncResult<T>> handler,
+			Object payload) {
+		request(HttpMethod.PUT, path, handler,  payload, null);
 
 	}
 
 	private <T> void request(final HttpMethod method, final String path, Handler<AsyncResult<T>> resultHandler,
-			final Class<T> responseClass, final Object payload, JsonObject queryParam) {
+			 final Object payload, JsonObject queryParam) {
 
 		if (Vertx.currentContext() == ctx) {
-			requestOnContext(method, path, resultHandler, responseClass, payload, queryParam);
+			requestOnContext(method, path, resultHandler,  payload, queryParam);
 		} else {
-			ctx.runOnContext(action -> request(method, path, resultHandler, responseClass, payload, queryParam));
+			ctx.runOnContext(action -> request(method, path, resultHandler,  payload, queryParam));
 		}
 
 	}
 
 	private <T> void requestOnContext(final HttpMethod method, final String path, Handler<AsyncResult<T>> resultHandler,
-			final Class<T> responseClass, final Object payload, JsonObject queryParam) {
+			 final Object payload, JsonObject queryParam) {
 
 		HttpRequest<Buffer> request = client.request(method, path);
 		request.putHeader("Authorization", "Bearer " + options.getGoogleCrendiatlToken());
